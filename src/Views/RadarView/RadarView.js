c5b0d878f585f4435f1b06dc72236df07f6992dd
Magnus <krummelur@gmail.com>
2019-12-18 19:20:36 +0100

    restructure

--- src/Views/RadarView/RadarView.js
+++ src/Views/RadarView/RadarView.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../../Components/Header/Header";
 import RadarContainer from "../../Components/Radar/RadarContainer";
+import SmallMapContainer from "../../Components/smallMap/SmallMapContainer";
 import RadarMenuContainer from "../../Components/RadarMenu/RadarMenuContainer";
 import "./RadarView.css";
 import Menu from "../../Components/Menu/Menu";
@@ -13,7 +14,10 @@ class RadarView extends Component {
 
                 <div className="RadarView">
                     <Header />
+                    <div className="previewContainer">
                     <RadarContainer />
+                    <SmallMapContainer />
+                    </div>
                     <RadarMenuContainer />
                 </div>
             </div>
