e05806ff0e22c4ea43e758d201601c859f8fa4dc
Magnus <krummelur@gmail.com>
2019-12-20 17:00:02 +0100

    set the smallmap center to match the zxy tile center

--- src/Components/SmallMap/SmallMap.js
+++ src/Components/SmallMap/SmallMap.js
@@ -2,15 +2,18 @@ import {GoogleApiWrapper, Map, Marker} from "google-maps-react";
 import React, { Component } from "react";
 import * as Constants from "../../data/apiConfig";
 import "../Radar/Radar.css"
+import { lonLatZoomToZXY, zxyToTileCenter } from '../../helpers/mapHelpers'
 
 class SmallMap extends Component {
 
     render() {
+        let smallMapCenter = lonLatZoomToZXY(this.props.radarCenter)
+        smallMapCenter = zxyToTileCenter(smallMapCenter);
         return (
             <div>
                 <Map google={this.props.google}
                      zoom={13}
-                     initialCenter={{lat: this.props.radarCenter.lat, lng: this.props.radarCenter.lon}}
+                     initialCenter={{lat: smallMapCenter.lat, lng: smallMapCenter.lon}}
                      style={{width: '512px', height: '512px'}}
                      disableDefaultUI={true}
                      gestureHandling={'none'}>
--- src/helpers/mapHelpers.js
+++ src/helpers/mapHelpers.js
@@ -6,6 +6,18 @@ export const lonLatZoomToZXY = ({ lon, lat, z = 12 }) => {
     return { z: z, x: Math.floor(xtile), y: Math.floor(ytile), xRem: fract(xtile), yRem: fract(ytile) };
 }
 
+export const zxyToTileCenter = ({z,x,y}) => {
+    x = x+0.5;
+    y = y+0.5;
+    let n = Math.pow(2, z);
+    let lon_deg = x / n * 360.0 - 180.0
+    let lat_rad = Math.atan(Math.sinh(Math.PI * (1 - 2 * y / n)))
+    let lat_deg = radToDeg(lat_rad)
+    return({lon: lon_deg, lat: lat_deg})
+}
+
 const fract = (num) => num - Math.trunc(num)
 export const degToRad = (a) =>  a * (Math.PI / 180.0);
+const radToDeg = (a) => a * (180.0 / Math.PI);
+
 const sec = (a) => 1/Math.cos(a)
