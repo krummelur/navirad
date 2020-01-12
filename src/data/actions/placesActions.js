28ca9b4bf7b20663525cb41b108c0a930107e8c9
Magnus <krummelur@gmail.com>
2020-01-12 08:20:22 +0100

    No empty location names allowed

--- src/data/actions/placesActions.js
+++ src/data/actions/placesActions.js
@@ -22,49 +22,54 @@ export const fetchPlacesAction = () => {
 
 export const addPlaceAction = (place) => {
   console.log("Adding place")
-  if(typeof place.name !== 'string') 
+  if (typeof place.name !== 'string')
     throw new Error("name must be string!");
+
   const { currentUser } = firebaseApp.auth();
   const childObject = {};
-  childObject[place.name] = {lat: place.lat, lon: place.lon}
+  childObject[place.name] = { lat: place.lat, lon: place.lon }
   return dispatch => {
-    firebaseApp
-    .database()
-    .ref(`places/${currentUser.uid}`)
-    .update(childObject, function(error){
-      if(error){
-        dispatch(showErrorAction(`could not save location ${place.name}!`))
-        dispatch(addPlaceFailureAction(place));
-      }
-      else{
-        dispatch(showMessageAction(`Location ${place.name} saved!`))
-        dispatch(addPlaceSuccessAction(place));
-      }
-    })
+    if (place.name === "")
+      dispatch(showErrorAction("Enter a name for the location first!"))
+    else {
+      firebaseApp
+        .database()
+        .ref(`places/${currentUser.uid}`)
+        .update(childObject, function (error) {
+          if (error) {
+            dispatch(showErrorAction(`could not save location ${place.name}!`))
+            dispatch(addPlaceFailureAction(place));
+          }
+          else {
+            dispatch(showMessageAction(`Location ${place.name} saved!`))
+            dispatch(addPlaceSuccessAction(place));
+          }
+        })
+    }
   }
 }
 
 export const removePlaceAction = (place) => {
   console.log("Adding place")
-  if(typeof place.name !== 'string') 
+  if (typeof place.name !== 'string')
     throw new Error("name must be string!");
   const { currentUser } = firebaseApp.auth();
   return dispatch => {
     firebaseApp
-    .database()
-    .ref(`places/${currentUser.uid}/${place.name}`)
-    .remove()
-    .then(function() {
-      dispatch(showMessageAction(`Location ${place.name} removed!`))
-      dispatch(removePlaceSuccessAction(place));
-    })
-    .catch(function(error) {
-      dispatch(showErrorAction(`could not delete location ${place.name}!`))
-      console.log("Remove failed: " + error.message)
-    });
+      .database()
+      .ref(`places/${currentUser.uid}/${place.name}`)
+      .remove()
+      .then(function () {
+        dispatch(showMessageAction(`Location ${place.name} removed!`))
+        dispatch(removePlaceSuccessAction(place));
+      })
+      .catch(function (error) {
+        dispatch(showErrorAction(`could not delete location ${place.name}!`))
+        console.log("Remove failed: " + error.message)
+      });
   }
 }
 
-const addPlaceSuccessAction = (newPlace) => { return { type: constants.ADD_PLACE_SUCCESS, payload: newPlace }};
-const addPlaceFailureAction = (error) => { return { type: constants.ADD_PLACE_FAILURE, payload: error }};
-const removePlaceSuccessAction = (removedPlace) => { return { type: constants.REMOVE_PLACE_SUCCESS, payload: removedPlace}};
+const addPlaceSuccessAction = (newPlace) => { return { type: constants.ADD_PLACE_SUCCESS, payload: newPlace } };
+const addPlaceFailureAction = (error) => { return { type: constants.ADD_PLACE_FAILURE, payload: error } };
+const removePlaceSuccessAction = (removedPlace) => { return { type: constants.REMOVE_PLACE_SUCCESS, payload: removedPlace } };
