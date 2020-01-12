9f3e7d2c97118ac498f5f6aa28c338d6f4573b91
adamcl <adamlil@kth.se>
2020-01-12 19:59:30 +0100

    Merge branch 'master' of C:\Users\adaml\Dropbox\Studier\KTH\DH2642 Interaktionsprogrammering\dh2642-proj with conflicts.

--- src/Components/Map/MapEmbedder.js
+++ src/Components/Map/MapEmbedder.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import { GoogleApiWrapper, Map, Marker } from "google-maps-react";
+import React, {Component} from "react";
+import {GoogleApiWrapper, Map, Marker} from "google-maps-react";
 import "./Map.css";
 import * as Constants from "../../data/apiConfig";
 import InfoWindowWrapper from "./InfoWindowWrapper";
-import { Link } from "react-router-dom";
-import { isEqual } from 'lodash';
+import {Link} from "react-router-dom";
+import {isEqual} from 'lodash';
 
 class MapEmbedder extends Component {
     constructor(props) {
@@ -12,45 +12,44 @@ class MapEmbedder extends Component {
         this.mapOptions = this.mapOptions.bind(this);
         this.state = {
             displayMarkerInfo: false,
-            isLoadingWaterApi: false
+            awaitingOnWater: true,
         }
     }
 
     centerMarker() {
-        return <Marker key="Marker" position={{ lat: this.props.radarCenter.lat, lng: this.props.radarCenter.lon }} />
+            return <Marker key="Marker" position={{ lat: this.props.radarCenter.lat, lng: this.props.radarCenter.lon }} />
     }
 
     infoBox() {
-        let infoBoxBody = this.state.isLoadingWaterApi ? 
-        <p> Loading... </p>
-        :
-        <React.Fragment>
-        <p>Open radar view at this position?</p>
-        <p>Longitude: {this.props.radarCenter.lon.toFixed(4)}</p>
-        <p>Latitude: {this.props.radarCenter.lat.toFixed(4)}</p>
-        </React.Fragment>
-        return (
-            //The InfoWindow will not trigger custom onClick events, hence this wrapper.
-            <InfoWindowWrapper
-                options={{
-                    pixelOffset: new window.google.maps.Size(0, -40)
-                }}
-                visible={this.state.displayMarkerInfo}
-                position={{ lat: this.props.radarCenter.lat, lng: this.props.radarCenter.lon }}
-                onClose={() => { this.setState({ displayMarkerInfo: false }) }}>
-                <div>
-                    <div className="infowindow-text">
-                        {infoBoxBody}
+        let waterOrNot;
+        if(this.state.awaitingOnWater) {
+            waterOrNot = this.onWater(this.props.radarCenter.lat, this.props.radarCenter.lon)
+        }
+        
+            return (
+                //The InfoWindow will not trigger custom onClick events, hence this wrapper.
+                <InfoWindowWrapper
+                    options={{
+                        pixelOffset: new window.google.maps.Size(0, -40)
+                    }}
+                    visible={this.state.displayMarkerInfo}
+                    position={{ lat: this.props.radarCenter.lat, lng: this.props.radarCenter.lon }}
+                    onClose={() => { this.setState({ displayMarkerInfo: false }) }}>
+                    <div>
+                        <div className="infowindow-text">
+                            <p>Open radar view at this position?</p>
+                            <p>Longitude: {this.props.radarCenter.lon.toFixed(4)}</p>
+                            <p>Latitude: {this.props.radarCenter.lat.toFixed(4)}</p>
+                        </div>
+                        <div className="center-aligned-element">
+                            <button type="button"
+                                //There seems to be an issue with using Links inside Map, and especially InfoWindow components.
+                                //This works, but could be made nicer. navigation by dispatch (redux-router) could probably solve it.
+                                    onClick={() => { document.getElementById("radarLink").click() }}>
+                                Go to radar view</button>
+                        </div>
                     </div>
-                    <div className="center-aligned-element">
-                        <button type="button"
-                            //There seems to be an issue with using Links inside Map, and especially InfoWindow components.
-                            //This works, but could be made nicer. navigation by dispatch (redux-router) could probably solve it.
-                            onClick={() => { document.getElementById("radarLink").click() }}>
-                            Go to radar view</button>
-                    </div>
-                </div>
-            </InfoWindowWrapper>)
+                </InfoWindowWrapper>)
     }
 
     mapOptions(mapProps, map) {
@@ -68,16 +67,13 @@ class MapEmbedder extends Component {
 
     componentDidUpdate(prevProps) {
         //If another component updates the center, we should move there and display info
-        if (!isEqual(prevProps.radarCenter, this.props.radarCenter) && !this.state.displayMarkerInfo) {
-            this.setState({ displayMarkerInfo: true })
+        if(!isEqual(prevProps.radarCenter, this.props.radarCenter) && !this.state.displayMarkerInfo){
+            this.setState({displayMarkerInfo: true})
         }
     }
 
     onWater = (lat, lon) => {
-        this.setState({ isLoadingWaterApi: true })
-        return fetch("https://api.onwater.io/api/v1/results/" + lat + "," + lon + "?access_token=" + Constants.ONWATER_API_KEY)
-            .then(response => response.json())
-            .then(response => response.water)
+      return fetch("https://api.onwater.io/api/v1/results/" + lat + "," + lon + "?access_token=" + Constants.ONWATER_API_KEY).then(response => response.json()).then(response => response.water).then(console.log)
     };
 
     render() {
@@ -89,18 +85,14 @@ class MapEmbedder extends Component {
                 <Map google={this.props.google}
                     className="google-maps-main"
                     zoom={12}
-                    initialCenter={{ lat: this.props.radarCenter.lat, lng: this.props.radarCenter.lon }}
+                    initialCenter={{lat: this.props.radarCenter.lat, lng: this.props.radarCenter.lon}}
                     disableDoubleClickZoom={true}
                     disableDefaultUI={true}
                     onReady={this.mapOptions}
                     style={{ width: '90%', height: '75%' }}
-                    onClick={(t, map, c) => {
-                        this.setState({ isLoadingWaterApi: true, displayMarkerInfo: true });
+                    onClick= {(t, map, c) => {
+                        this.setState({displayMarkerInfo: true});
                         this.props.setRadarCenter({ lon: c.latLng.lng(), lat: c.latLng.lat() })
-                        this.onWater(c.latLng.lat(), c.latLng.lng()).then((result) => {
-                            //TODO CHECK AND HANDLE ANY API ERRORS!!
-                            this.setState({ isLoadingWaterApi: false });
-                        })
                     }}>
                     {this.state.displayMarkerInfo && this.centerMarker()}
                     {this.infoBox()}
